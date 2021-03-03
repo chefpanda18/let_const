08a4f72d5239896a3f9427008a4687fd3af6be09
Amanda <75429795+chefpanda18@users.noreply.github.com>
2021-03-03 15:49:02 -0500

    Add files via upload

---
+++ let_const.js
@@ -0,0 +1,19 @@
+// ES2015 Global Constants
+
+const PI = 3.14;
+// if you try to reassing PI it will give you an error
+
+// What is the difference between var and let?
+// var is scoped to the function, can be reassigned and redeclared and manipulated
+// let is scoped to the block, can be reassigned but not redeclared and can be manipulated
+
+// What is the difference between var and const?
+// const cannot be reassigned or redeclared but can be manipulated
+// var can be reassigned and redeclared
+
+// What is the difference between let and const?
+// let can be reassigned and const cannot
+
+// What is hoisting?
+// var can be used outside of the scope of where it is in the code because of the way JS reads the code
+// function declarations are invoked before other code is read
